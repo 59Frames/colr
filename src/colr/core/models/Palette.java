8740e9f347b861ef5613ef79c8a2c7f2c02bc512
Daniel <daniel.seifert@nodflow.ch>
2018-09-05 21:32:29 +0200

    implemented palettes

---
+++ src/colr/core/models/Palette.java
@@ -0,0 +1,48 @@
+package colr.core.models;
+
+public class Palette {
+    private final Color[] colors;
+
+    private String title;
+
+    public Palette(String title) {
+        this.colors = new Color[6];
+        this.title = title;
+    }
+
+    public Color add(Color color) {
+        for (int i = 0; i < colors.length; i++) {
+            if (colors[i] == null){
+                colors[i] = color;
+                return color;
+            }
+        }
+
+        System.out.println("Palette is full already... please remove a color");
+        return null;
+    }
+
+    public Color get(int index) {
+        return colors[index];
+    }
+
+    public Color remove(int index) {
+        if (0 <= index && index < 6){
+            Color c = colors[index];
+            colors[index] = null;
+            return c;
+        } else {
+            System.out.println("Invalid index");
+            return null;
+        }
+    }
+
+    public Color remove(Color color) {
+        for (int i = 0; i < colors.length; i++) {
+            if (colors[i] == color)
+                return remove(i);
+        }
+
+        return null;
+    }
+}
